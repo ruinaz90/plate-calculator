b3e376a4f36a2963e84845ec5e842344a315e13d
ruinaz90 <rzhao9002@gmail.com>
2023-03-21 18:55:03 +0000

    plates

--- components/Plates.jsx
+++ components/Plates.jsx
@@ -1,14 +1,31 @@
 import FormGroup from '@mui/material/FormGroup'
 import FormControlLabel from '@mui/material/FormControlLabel'
 import Checkbox from '@mui/material/Checkbox'
+import { useState } from 'react'
 
 function Plates() {
+    const [plates, setPlates] = useState([])
+
+    function handleChange(e) {
+        const { value, checked } = e.target
+        const weight = Number(value)
+        console.log(`${value} is ${checked}`)
+
+        if(checked) {
+            setPlates([...plates, weight])
+        }
+        else {
+            setPlates(plates.filter(element => element !== weight))
+        }
+    }
+    console.log(plates)
+
     return (
         <div>
             <FormGroup>
-                <FormControlLabel control={<Checkbox defaultChecked />} label="2.5 lbs" id="plate2-5" />
-                <FormControlLabel control={<Checkbox defaultChecked />} label="5 lbs" id="plate5" />
-                <FormControlLabel control={<Checkbox defaultChecked />} label="10 lbs" id="plate10" />
+                <FormControlLabel control={<Checkbox />} label="2.5 lbs" onChange={handleChange} value="2.5" />
+                <FormControlLabel control={<Checkbox />} label="5 lbs" onChange={handleChange} value="5" />
+                <FormControlLabel control={<Checkbox />} label="10 lbs" onChange={handleChange} value="10" />
             </FormGroup>
         </div>
     )
