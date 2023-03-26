648ca69418727fd5cb62144d2d9a81ee31f82d29
ruinaz90 <rzhao9002@gmail.com>
2023-03-26 21:07:20 +0000

    style fix

--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,17 @@ import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
 import { calculate as calculatePlate } from '@/public/plate-calculator'
+
 import FormGroup from '@mui/material/FormGroup'
 import FormControlLabel from '@mui/material/FormControlLabel'
 import Checkbox from '@mui/material/Checkbox'
 import TextField from '@mui/material/TextField'
 import Button from '@mui/material/Button'
 import Typography from '@mui/material/Typography'
+import Card from '@mui/material/Card'
+import CardContent from '@mui/material/CardContent'
+import Container from '@mui/material/Container'
+
 import { useState } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -56,22 +61,28 @@ export default function Home() {
             {/*<link rel="icon" href="/favicon.ico" />*/}
         </Head>
         <main className={styles.main}>
-            <FormGroup>
-                <FormControlLabel control={<Checkbox />} label="2.5 lbs" onChange={handleChange} value="2.5" />
-                <FormControlLabel control={<Checkbox defaultChecked />} label="5 lbs" onChange={handleChange} value="5" />
-                <FormControlLabel control={<Checkbox defaultChecked />} label="10 lbs" onChange={handleChange} value="10" />
-                <FormControlLabel control={<Checkbox defaultChecked />} label="25 lbs" onChange={handleChange} value="25" />
-                <FormControlLabel control={<Checkbox defaultChecked />} label="35 lbs" onChange={handleChange} value="35" />
-                <FormControlLabel control={<Checkbox defaultChecked />} label="45 lbs" onChange={handleChange} value="45" />
-                <TextField sx={{ m: 1, width: '25ch' }} label="Weight (lbs)" variant="standard" type="number" value={weight} onChange={(e) => setWeight(e.target.value)} />
-                <Button variant="contained" onClick={handleSubmit}>Calculate</Button>
-            </FormGroup>
+        <Container maxWidth="sm">
+            <Card sx={{ minWidth: 275 }}>
+            <CardContent>
+                <FormGroup>
+                    <FormControlLabel control={<Checkbox />} label="2.5 lbs" onChange={handleChange} value="2.5" />
+                    <FormControlLabel control={<Checkbox defaultChecked />} label="5 lbs" onChange={handleChange} value="5" />
+                    <FormControlLabel control={<Checkbox defaultChecked />} label="10 lbs" onChange={handleChange} value="10" />
+                    <FormControlLabel control={<Checkbox defaultChecked />} label="25 lbs" onChange={handleChange} value="25" />
+                    <FormControlLabel control={<Checkbox defaultChecked />} label="35 lbs" onChange={handleChange} value="35" />
+                    <FormControlLabel control={<Checkbox defaultChecked />} label="45 lbs" onChange={handleChange} value="45" />
+                    <TextField sx={{ m: 1, width: '25ch' }} label="Weight (lbs)" variant="standard" type="number" value={weight} onChange={(e) => setWeight(e.target.value)} />
+                    <Button variant="contained" onClick={handleSubmit}>Calculate</Button>
+                </FormGroup>
 
-            <Typography variant="h5">Closest weight</Typography>
-            <Typography variant="body1">{resultsWeight} lbs</Typography>
-            
-            <Typography variant="h5">Plates needed</Typography>
-            <Typography variant="body1">{results.length ? results.map((element, index) => <li key={index}>{element} lbs x {resultsQty[index]}</li>) : "None"}</Typography>
+                <Typography variant="h5">Closest weight</Typography>
+                <Typography variant="body1">{resultsWeight} lbs</Typography>
+                
+                <Typography variant="h5">Plates needed</Typography>
+                <div>{results.length ? results.map((element, index) => <Typography variant="body1" key={index}>{element} lbs x {resultsQty[index]}</Typography>) : <Typography variant="body1">None</Typography>}</div>
+            </CardContent>
+            </Card>
+            </Container>
         </main>
         </>
     )
